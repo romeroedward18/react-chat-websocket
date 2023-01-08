0964948246458c93a521ddeda9450359580ffcc5
romeroedward18 <romero.edward18@gmail.com>
2023-01-08 16:39:25 -0500

    Spinner y validación conexión websocket

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import * as dayjs from "dayjs";
 import "dayjs/locale/es";
 import { io } from "socket.io-client";
 import ModalForm from "./components/ModalForm";
+import Spinner from "react-bootstrap/Spinner";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -41,6 +42,7 @@ function App() {
   ];
   // Se inicializa una referencia de audio y los estados de la aplicación
   const audioPlayer = useRef(null);
+  const [connected, setConnected] = useState(false);
   const [modalShow, setModalShow] = useState(true);
   const [userData, setUserData] = useState({});
   const [activeChat, setActiveChat] = useState(null);
@@ -52,54 +54,58 @@ function App() {
   }
 
   useEffect(() => {
-    // Cuando se recibe un mensaje de "receive-subscribe", actualiza la lista de usuarios
-    socket.on("receive-subscribe", (userList) => {
-      // Filtramos la lista de usuarios para excluir al usuario actual
-      const userListFilter = Object.values(userList).reduce((acc, user) => {
-        if (user.id !== socket.id) {
-          acc.push(user);
-        }
-        return acc;
-      }, []);
-      // Actualizamos el estado de la aplicación con la lista de usuarios filtrada
-      setChat((chats) => {
-        let updatedChats = [...chats];
-        // Añadimos cada usuario a la lista de chats si no está ya en ella
-        userListFilter.forEach((user) => {
-          if (!updatedChats.find((chat) => chat.id === user.id)) {
-            updatedChats.push(user);
+    socket.on("connect", () => {
+      setConnected(true);
+      // Cuando se recibe un mensaje de "receive-subscribe", actualiza la lista de usuarios
+      socket.on("receive-subscribe", (userList) => {
+        // Filtramos la lista de usuarios para excluir al usuario actual
+        const userListFilter = Object.values(userList).reduce((acc, user) => {
+          if (user.id !== socket.id) {
+            acc.push(user);
           }
+          return acc;
+        }, []);
+        // Actualizamos el estado de la aplicación con la lista de usuarios filtrada
+        setChat((chats) => {
+          let updatedChats = [...chats];
+          // Añadimos cada usuario a la lista de chats si no está ya en ella
+          userListFilter.forEach((user) => {
+            if (!updatedChats.find((chat) => chat.id === user.id)) {
+              updatedChats.push(user);
+            }
+          });
+          // Filtramos la lista de chats para excluir a cualquier usuario que ya no esté en la lista de usuarios
+          updatedChats = updatedChats.filter((chat) => userList[chat.id]);
+          // Mantenemos el usuario de pruebas en la lista de chats
+          const chat1 = chats.find((chat) => chat.id === 1);
+          if (chat1 && !updatedChats.find((chat) => chat.id === 1)) {
+            updatedChats.push(chat1);
+          }
+          return updatedChats;
         });
-        // Filtramos la lista de chats para excluir a cualquier usuario que ya no esté en la lista de usuarios
-        updatedChats = updatedChats.filter((chat) => userList[chat.id]);
-        // Mantenemos el usuario de pruebas en la lista de chats
-        const chat1 = chats.find((chat) => chat.id === 1);
-        if (chat1 && !updatedChats.find((chat) => chat.id === 1)) {
-          updatedChats.push(chat1);
-        }
-        return updatedChats;
       });
-    });
 
-    // Cuando se recibe un mensaje de "receive-message", se actualiza el chat activo y reproducimos un sonido de notificación
-    socket.on("receive-message", (msgObj) => {
-      setChat((chats) =>
-        chats.map((chat) => {
-          if (chat.id !== activeChat) {
-            chat.status = "unseen";
-            playAudio();
-          }
-          // Si el id del chat coincide con el id del usuario que envió el mensaje, actualizamos el objeto con el nuevo mensaje
-          if (chat.id === msgObj.userId) {
-            return { ...chat, messages: [...chat.messages, msgObj] };
-          }
-          return chat;
-        })
-      );
+      // Cuando se recibe un mensaje de "receive-message", se actualiza el chat activo y reproducimos un sonido de notificación
+      socket.on("receive-message", (msgObj) => {
+        setChat((chats) =>
+          chats.map((chat) => {
+            if (chat.id !== activeChat) {
+              chat.status = "unseen";
+              playAudio();
+            }
+            // Si el id del chat coincide con el id del usuario que envió el mensaje, actualizamos el objeto con el nuevo mensaje
+            if (chat.id === msgObj.userId) {
+              return { ...chat, messages: [...chat.messages, msgObj] };
+            }
+            return chat;
+          })
+        );
+      });
     });
 
     // Desconectamos los handlers cuando se desmonta el componente
     return () => {
+      socket.off("connect");
       socket.off("receive-subscribe");
       socket.off("receive-message");
     };
@@ -162,36 +168,45 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ModalForm
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-          joinChat={joinChat}
-        />
-        {!modalShow ? (
-          <Container className="chat-container">
-            <Row>
-              <Col className="chat-box user" xs>
-                <UserList
-                  currentChat={currentChat}
-                  chats={sortChatsByLastMessage(chats)}
-                  joinChannel={joinChannel}
-                  userData={userData}
-                />
-              </Col>
-              <Col className="chat-box msg" xs lg="8">
-                <MessageContainer
-                  userData={userData}
-                  currentChat={currentChat}
-                  chats={chats}
-                />
-                {currentChat ? (
-                  <InputMessage sendMessage={sendMessage} />
-                ) : null}
-              </Col>
-            </Row>
-            <audio ref={audioPlayer} src={NotificationSound} />
-          </Container>
-        ) : null}
+        {connected ? (
+          <>
+            <ModalForm
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+              joinChat={joinChat}
+            />
+            {!modalShow ? (
+              <Container className="chat-container">
+                <Row>
+                  <Col className="chat-box user" xs>
+                    <UserList
+                      currentChat={currentChat}
+                      chats={sortChatsByLastMessage(chats)}
+                      joinChannel={joinChannel}
+                      userData={userData}
+                    />
+                  </Col>
+                  <Col className="chat-box msg" xs lg="8">
+                    <MessageContainer
+                      userData={userData}
+                      currentChat={currentChat}
+                      chats={chats}
+                    />
+                    {currentChat ? (
+                      <InputMessage sendMessage={sendMessage} />
+                    ) : null}
+                  </Col>
+                </Row>
+                <audio ref={audioPlayer} src={NotificationSound} />
+              </Container>
+            ) : null}
+          </>
+        ) : (
+          <div className="spinner-container">
+            <Spinner className="spinner-icon" variant="light"></Spinner>
+            <span>Cargando...</span>
+          </div>
+        )}
       </header>
     </div>
   );
